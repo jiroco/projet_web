676c90394b0fc48a0bb7857bb6d640cd316fed44
jean savoldelli <jean savoldelli>
2017-05-08 14:18:20 +0200

    fonction register en fonctionnement

---
+++ register.inc.php
@@ -0,0 +1,30 @@
+<?php
+$DBcon = new PDO('mysql:host=localhost;dbname=insatwitter;charset=utf8','root','');
+
+if(isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['prenom']) && !empty($_POST['prenom']) && isset($_POST['nom']) && !empty($_POST['nom']) && isset($_POST['passwd']) && !empty($_POST['passwd']) && isset($_POST['username']) && !empty($_POST['username'])){
+	$email=htmlspecialchars($_POST['email']);
+	$nom=htmlspecialchars($_POST['nom']);
+    $prenom=htmlspecialchars($_POST['prenom']);
+    $passwd=htmlspecialchars($_POST['passwd']);
+    $passwd=sha1($passwd);
+    $username=htmlspecialchars($_POST['username']);
+	$req = $DBcon->prepare('INSERT INTO `user`(`ID`, `NOM`, `PRENOM`, `MAIL`, `USERNAME`, `PASSWORD`) VALUES (NULL,?,?,?,?,?)');
+					
+    $req->bindValue(1, $nom, PDO::PARAM_STR);
+    $req->bindValue(2, $prenom, PDO::PARAM_STR);
+    $req->bindValue(3, $email, PDO::PARAM_STR);
+    $req->bindValue(4, $username, PDO::PARAM_STR);
+    $req->bindValue(5, $passwd, PDO::PARAM_STR);
+
+    $check=$req->execute();
+    
+    if($check){
+        include("connexion.inc.php");
+        header('Location: accueil_connecte.php');
+    }
+    else
+        echo "Un problème s'est produit lors de la requete.<br>";
+    
+    $req->closeCursor();
+}
+?>
--- register.php
+++ register.php
@@ -1,20 +1,20 @@
 <?php 
 include("head.php") 
-//include("include/bdd_connexion.php");
 //session_start(); ?>
 
 
 <div >
-	<form action="accueil.php" method="POST">
+	<form action="register.php" method="POST">
 		<p>Inscription</p>
 	    <input type="hidden" name="role"/>
 		Adresse :<input type="email" name="email" placeholder="Entrer votre adresse mail"/><br>
 		Prénom :<input type="text" name="prenom" placeholder="Entrer votre prénom"/><br>
 		Nom :<input type="text" name="nom" placeholder="Entrer votre nom"/><br>
+		Username:<input type="text" name="username" placeholder="Entrer votre username"/><br>
 		Mot de passe:	<input type="password" name="passwd" placeholder="Mot de passe"/><br>
-		Confirmation du mot de passe: <input type="password" name="passwd2" placeholder="Confirmation mot de passe" /><br> <br><br> <br>
 		<input type="submit" value="S'inscrire"/>	
 	</form>
 </div>
-
+<?php
+include("register.inc.php");
 ?>
