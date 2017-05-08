79dd9048625031cb7bc800fdc7f5fb1aa9e94ed2
jean savoldelli <jean savoldelli>
2017-05-08 11:53:17 +0200

    création de la page publication_message

--- accueil_connecte.php
+++ accueil_connecte.php
@@ -1,11 +1,20 @@
 <?php
 session_start();
-
-
 if ((!isset($_SESSION['id'])) || (empty($_SESSION['id']))){
     echo "<meta http-equiv='refresh' content='0; URL=index.php'>";
 }
+?>
 
+<div>
+	<form action="accueil_connecte.php" method="POST">
+			<p>Publication</p>
+			<input type="hidden" name="role"/>
+			Contenu :<input type="text" name="textpublie" placeholder="Entrer votre texte"/><br>
+			<input type="submit" value="publier">
+	</form>
+</div>
+
+<?php
 include("publication_abonnees.php");
 include("publication_message.php");
 ?>
---
+++ publication_message.php
@@ -0,0 +1,22 @@
+<?php
+$DBcon = new PDO('mysql:host=localhost;dbname=insatwitter;charset=utf8','root','');
+
+if(isset($_POST['textpublie']) && !empty($_POST['textpublie'])){
+	$text=htmlspecialchars($_POST['textpublie']);
+	
+	$req = $DBcon->prepare('INSERT INTO `messages`(`ID`, `IDUSER`, `CONTENU`) VALUES (NULL,?,?)');
+					
+    $req->bindValue(1, $_SESSION['id'], PDO::PARAM_STR);
+    $req->bindValue(2, $text, PDO::PARAM_STR);
+
+    $check=$req->execute();
+    
+    if($check){
+        header('Location: accueil_connecte.php');
+    }
+    else
+        echo "Un problème s'est produit lors de la requete.<br>";
+    
+    $req->closeCursor();
+}
+?>
