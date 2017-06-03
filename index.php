779c2c6de8733b5984ef393b938297552f42aa6b
ClaireHmt <claire.hamonet2013@laposte.net>
2017-06-03 18:57:32 +0200

    refonte accueil avec navBar 	supprimé :        htmlpageLoginRegister-01.html 	modifié :         index.php 	index.css

--- index.php
+++ index.php
@@ -1,12 +1,56 @@
-<?php 
-include("head.php") ;
-?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
 
-<div>
-   	<div id = "connexion">
-		<a href="login.php">Connexion</a>
-	</div>
-	<div id="inscription">
-		<a href="register.php">Inscription</a>
-	</div>
-</div>
+    <title>Accueil</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="bootstrap/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="bootstrap/css/starter-template.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>-->
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" type="text/css" href="register.css">
+  </head>
+
+  <body>
+<?php include("htmlnavBar.php")?>
+
+    <div class="container">
+      <div class="starter-template">
+        <h1>INSA'Twitter</h1>
+        <p class="lead">Un Twitter réservé à l'INSA !!!</p>
+      </div>
+
+      <div class="bouton">
+      <form methode="POST" action="login.php">
+        <button type="submit" class="btn btn-XL btn-default">Login</button>
+        </form>
+        <br><br>
+        <form methode="POST" action="register.php">
+        <button type="submit" class="btn btn-XL btn-default">Register</button>
+        </form>
+
+      </div>
+  </body>
+</html>
+
