e881171a8664a22a72e924194a2ea57988eef59c
ClaireHmt <claire.hamonet2013@laposte.net>
2017-06-06 18:47:02 +0200

    modifié :         deconnexion.php 	modifié :         htmlabout.php 	modifié :         include/connexion.inc.php 	modifié :         include/connexiondb.php 	htmlpageAccueil.php~HEAD 	index.css 	insatwitter.sql.zip

--- include/connexion.inc.php
+++ include/connexion.inc.php
@@ -2,12 +2,14 @@
 include("connexiondb.php");
 if (isset($_POST['username']) && !empty($_POST['username']) && isset($_POST['passwd']) && !empty($_POST['passwd']) && is_string($_POST['username']) && is_string($_POST['passwd'])){
         
-        $username=htmlspecialchars($_POST['username']);
-        $pass=htmlspecialchars(sha1($_POST['passwd']));
+        $username=htmlspecialchars($_POST['username']); //Certains caractères ont des significations spéciales en HTML, et doivent être remplacés par des entités HTML pour conserver leurs significations.
+        $pass=htmlspecialchars(sha1($_POST['passwd']));//hachage
         $req = $DBcon->prepare('SELECT ID, USERNAME, PASSWORD, MAIL, NOM, PRENOM, DEPARTEMENT from user WHERE USERNAME = ? ');
         $req->bindValue(1,$username,PDO::PARAM_STR);
-        $check=$req->execute();  
+        $check=$req->execute(); 
+        echo $check;
         if($check){
+            echo "coucou"; 
             if($donnee = $req->fetch()){
                 if($pass == $donnee['PASSWORD']){
                     session_start(); 
@@ -17,7 +19,7 @@ if (isset($_POST['username']) && !empty($_POST['username']) && isset($_POST['pas
                     $_SESSION['prenom']=$donnee['PRENOM'];
                     $_SESSION['username']= $donnee['USERNAME'];
                     $_SESSION['departement']= $donnee['DEPARTEMENT'];
-                    echo "<meta http-equiv='refresh' content='0; URL=accueil_connecte.php'>";    
+                    echo "c'est bon <meta http-equiv='refresh' content='0; URL=htmlpageAccueil.php'>";    
                 }
                 else
                     echo "Mauvais identifiants.<br>";
