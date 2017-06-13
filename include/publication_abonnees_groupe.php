c5f8d251bba22d35878139d196206b831a55b638
david.neyron <david.neyron@gmail.com>
2017-06-13 10:59:14 +0200

    Like pour les groupes -> importez new BDD

--- include/publication_abonnees_groupe.php
+++ include/publication_abonnees_groupe.php
@@ -2,9 +2,9 @@
 
 include("connexiondb.php");
 
-$req = $DBcon->prepare('SELECT MESSAGE, NOM, PRENOM, IMAGE from messagetog WHERE IDGROUPE = ? GROUP BY ID DESC'); 
+$req = $DBcon->prepare('SELECT ID, THUMBSUP, MESSAGE, NOM, PRENOM, IMAGE from messagetog WHERE IDGROUPE = ? GROUP BY ID DESC');
 $req->bindValue(1,$_SESSION['IDGROUPE'],PDO::PARAM_INT);
-$check=$req->execute(); 
+$check=$req->execute();
 if($check){
 	while ($resultat=$req->fetch()){
 		?>
@@ -16,8 +16,11 @@ if($check){
 				<div class="media-body">
 
 				    <h4 class="media-heading"><br/> <?php print_r($resultat["NOM"]." ".$resultat["PRENOM"]) ?> </h4>
-					<?php print_r($resultat["MESSAGE"]."<br/>");?>
-				<br/>
+							<?php print_r($resultat["MESSAGE"]."<br/>");?>
+						<br/>
+						<input id="<?php echo $resultat["ID"]; ?>" class="jaimeb" type="button" value="J'aime">
+						<label id="<?php echo -$resultat["ID"]; ?>"> J'aime : <?php echo $resultat["THUMBSUP"]; ?> </label>
+					<br/>
 				</div>
 			</div>
    		</div> <!--panel 1 fin-->
