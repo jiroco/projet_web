8cda0bf54e2f3cc867378491804f7cd0a4c33c77
david.neyron <david.neyron@gmail.com>
2017-06-18 23:04:37 +0200

    parametre updated

--- include/parametre.inc.php
+++ include/parametre.inc.php
@@ -11,10 +11,10 @@ if ((isset($_POST['nom']) && !empty($_POST['nom'])) ||
   (isset($_POST['password']) && !empty($_POST['password']))) {
 
   if ($_POST['password'] != NULL) {
-    $change = $DBcon->prepare('UPDATE user SET PASSWORD = ? WHERE ID = ?');
-    $change->bindValue(1,strip_tags(sha1($_POST['password'])),PDO::PARAM_INT);
-    $change->bindValue(2,$_SESSION["id"],PDO::PARAM_INT);
-    $change->execute();
+    $change_user = $DBcon->prepare('UPDATE user SET PASSWORD = ? WHERE ID = ?');
+    $change_user->bindValue(1,strip_tags(sha1($_POST['password'])),PDO::PARAM_INT);
+    $change_user->bindValue(2,$_SESSION["id"],PDO::PARAM_INT);
+    $change_user->execute();
     echo "Votre mot de passe a bien été changé ";
   }
   elseif ($_POST['username'] != NULL) {
@@ -39,6 +39,10 @@ if ((isset($_POST['nom']) && !empty($_POST['nom'])) ||
     $change->bindValue(2,$_SESSION["id"],PDO::PARAM_INT);
     $change->execute();
     $_SESSION['nom']=strip_tags($_POST['nom']);
+    $change_mess = $DBcon->prepare('UPDATE messages SET NOM = ? WHERE IDUSER = ?');
+    $change_mess->bindValue(1,$_POST['nom'],PDO::PARAM_INT);
+    $change_mess->bindValue(2,$_SESSION["id"],PDO::PARAM_INT);
+    $change_mess->execute();
     echo "Votre nom a bien été changé";
   }
   elseif ($_POST['prenom'] != NULL) {
@@ -47,6 +51,10 @@ if ((isset($_POST['nom']) && !empty($_POST['nom'])) ||
     $change->bindValue(2,$_SESSION["id"],PDO::PARAM_INT);
     $change->execute();
     $_SESSION['prenom']=$_POST['prenom'];
+    $change_mess = $DBcon->prepare('UPDATE messages SET PRENOM = ? WHERE IDUSER = ?');
+    $change_mess->bindValue(1,$_POST['prenom'],PDO::PARAM_INT);
+    $change_mess->bindValue(2,$_SESSION["id"],PDO::PARAM_INT);
+    $change_mess->execute();
     echo "Votre prenom a bien été changé";
   }
 
